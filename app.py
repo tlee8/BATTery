49535f6cf1f3a12da02b8b29941429bb27d51301
Britni <bcanale@stuy.edu>
2018-11-28 21:37:41 -0500

    Used inheritance w Jinja, added new login html page (not functional)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, url_for, redirect, flash
+import os
 import json, urllib
 
 
@@ -9,7 +10,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("test.html", title = "DAILY BATT")
+    ''' Immediately redirects to login page; users must be logged in to use
+    '''
+    return redirect(url_for("login"))
+
+
+@app.route("/home", methods=["POST", "GET"])
+def home():
+    ''' Displays information from all APIs to logged in users
+    '''
+    return render_template("home.html", title = "DAILY BATT")
+
+@app.route("/login")
+def login():
+    ''' Allows user to login
+
+    Users must log in to access website
+    If a user does not have an account, they may sign up for one
+    '''
+    return render_template("login.html")
 
 if __name__== "__main__":
     app.debug = True
