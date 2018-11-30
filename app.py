616254c3599aec3956d2589dce51eb10540b5600
Britni <bcanale@stuy.edu>
2018-11-29 21:48:31 -0500

    trying to add alerts to webpage

--- app.py
+++ app.py
@@ -8,59 +8,57 @@ import json, urllib
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
+badcreds = False #boolean for login creds
+
 @app.route("/")
 def hello():
-    if ( (session.get('username') == "battery") ):
-        return render_template("home.html",
-                                user = "battery")
-    else:
-        return render_template( "login.html" )
     ''' Immediately redirects to login page; users must be logged in to use
     '''
+    if ( (session.get('username') == "battery") ):
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("login"))
     #return redirect(url_for("login"))
 
-@app.route("/logout")
-def logout():
-    try:
-        session.pop('username')
-        return render_template("login.html")
-    except:
-        return render_template("login.html")
 
-
-@app.route("/home", methods=["POST", "GET"])
-def home():
-    ''' Displays information from all APIs to logged in users
-    '''
-    return render_template("home.html", title = "DAILY BATT")
-
-@app.route("/login", methods = ["POST"])
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    if ((request.form['username'] == "battery") &
-        (request.form['password'] == "timiscool")):
-        session['username'] = "battery"
-        return render_template("home.html",
-                                user = "battery")
-    elif ((request.form['username']) == "battery"):
-        return render_template("wrongcreds.html",
-                                user = request.form['username'],
-                                pwd = request.form['password'],
-                                error = "Your password was incorrect")
-    elif ((request.form['password']) == "timiscool"):
-        return render_template("wrongcreds.html",
-                                user = request.form['username'],
-                                pwd = request.form['password'],
-                                error = "Your username was incorrect")
-    else:
-        return render_template("wrongcreds.html",
-                                user = request.form['username'],
-                                pwd = request.form['password'],
-                                error = "Your password and password were both incorrect!")
     ''' Allows user to login
 
     Users must log in to access website
     If a user does not have an account, they may sign up for one
     '''
+    print(badcreds)
+    return render_template("login.html", wrong = badcreds)
+
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    try:
+        session.pop('username')
+        return redirect(url_for("login"))
+    except:
+        return redirect(url_for("login"))
+
+@app.route("/home", methods=["POST", "GET"])
+def home():
+    ''' Displays information from all APIs to logged in users
+    '''
+    return render_template("home.html", title = "DAILY BATT", user = session.get('username'))
+
+@app.route("/auth", methods = ["POST"])
+def auth():
+
+    if ((request.form['username'] == "battery") and
+        (request.form['password'] == "timiscool")):
+        session['username'] = "battery"
+        badcreds = False
+        return redirect(url_for("home"))
+    else:
+        badcreds = True
+        print("noooooo")
+        print(badcreds)
+        return redirect(url_for("login"))
+
 
     #return render_template("login.html")'''
 
